34b253bf3070aa8efa256cab4657f2431affeb66
Anderson Severo <andersonsevero@gmail.com>
2024-07-04 15:09:44 -0300

    Identificação Render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  currentVersion: '240704-1-hostinger',
+  currentVersion: '240704-1-render',
   apiUrl: 'http://srv554980.hstgr.cloud:9090',
   mercadoPagoKey: 'APP_USR-c07e7870-3c00-49cc-a913-9d2f5b7dfa92',
   recomendationApiUrl: 'http://srv554980.hstgr.cloud:9001',
