1ccdd9940344f08a5884f0b34dbcdea3d1edc634
Anderson Severo <andersonsevero@gmail.com>
2024-07-04 21:08:04 -0300

    Fix: removida barra extra nas URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   currentVersion: '240704-2-hostinger',
-  apiUrl: 'https://backend-backend.5ndr18.easypanel.host/',
+  apiUrl: 'https://backend-backend.5ndr18.easypanel.host',
   mercadoPagoKey: 'APP_USR-c07e7870-3c00-49cc-a913-9d2f5b7dfa92',
-  recomendationApiUrl: 'https://ia-recomendations.5ndr18.easypanel.host/',
+  recomendationApiUrl: 'https://ia-recomendations.5ndr18.easypanel.host',
 };
