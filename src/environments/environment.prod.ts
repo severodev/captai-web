4de2392237773ffcd0a27b18cb2c57507526d5d5
Anderson Severo <andersonsevero@gmail.com>
2024-07-04 21:08:28 -0300

    ID Versão 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  currentVersion: '240704-2-hostinger',
+  currentVersion: '240704-3-hostinger',
   apiUrl: 'https://backend-backend.5ndr18.easypanel.host',
   mercadoPagoKey: 'APP_USR-c07e7870-3c00-49cc-a913-9d2f5b7dfa92',
   recomendationApiUrl: 'https://ia-recomendations.5ndr18.easypanel.host',
