2baad2f5adcabd1c27571020c977c797ace52755
Anderson Severo <andersonsevero@gmail.com>
2024-07-04 21:04:06 -0300

    URLs de back e ia atualizados (easypanel)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  currentVersion: '240704-1-hostinger',
-  apiUrl: 'http://srv554980.hstgr.cloud:9090',
+  currentVersion: '240704-2-hostinger',
+  apiUrl: 'https://backend-backend.5ndr18.easypanel.host/',
   mercadoPagoKey: 'APP_USR-c07e7870-3c00-49cc-a913-9d2f5b7dfa92',
-  recomendationApiUrl: 'http://srv554980.hstgr.cloud:9001',
+  recomendationApiUrl: 'https://ia-recomendations.5ndr18.easypanel.host/',
 };
