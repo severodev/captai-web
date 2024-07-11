642cd7f005adadb1e39335dbbf335de8c969c03b
Anderson Severo <andersonsevero@gmail.com>
2024-07-11 09:54:57 -0300

    Configurações ambiente Hostinger

---
+++ src/environments/environment.hostinger.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  currentVersion: '240711-1-hostinger',
+  apiUrl: 'https://backend-backend.5ndr18.easypanel.host',
+  mercadoPagoKey: 'APP_USR-c07e7870-3c00-49cc-a913-9d2f5b7dfa92',
+  recomendationApiUrl: 'https://ia-recomendations.5ndr18.easypanel.host',
+};
