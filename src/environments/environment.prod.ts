bc1f4b5fbf676b9f5d8d5bf3650e715494f01296
Anderson Severo <andersonsevero@gmail.com>
2024-07-11 09:56:25 -0300

    Env de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  currentVersion: '240704-3-hostinger',
+  currentVersion: '240711-1-prod',
   apiUrl: 'https://backend-backend.5ndr18.easypanel.host',
   mercadoPagoKey: 'APP_USR-c07e7870-3c00-49cc-a913-9d2f5b7dfa92',
   recomendationApiUrl: 'https://ia-recomendations.5ndr18.easypanel.host',
